d8f91be2d92d846632ea2bd8f15d9b1cefeec98c
Raivisor <georgijsmirnov159@gmail.com>
2025-12-17 00:14:01 +0300

    Building a menu project using Cmake

---
+++ multilevel_project_Cmake/build_module_app.sh
@@ -0,0 +1,14 @@
+BUILD_TYPE=Ninja
+BUILD_SUFFIX=ninja
+
+BUILD_FOLDER=build_$BUILD_SUFFIX
+SOURCE_FOLDER=projects
+
+if [ ! -e $BUILD_FOLDER ]; then
+	mkdir $BUILD_FOLDER
+fi
+
+cd $BUILD_FOLDER
+
+cmake -G "$BUILD_TYPE" "../$SOURCE_FOLDER"
+cmake --build .
---
+++ multilevel_project_Cmake/projects/menu/main.cpp
@@ -0,0 +1,13 @@
+#include "declare_menu_items.hpp"
+#include "menu_function.hpp"
+#include "menu_item.hpp"
+
+int main() {
+
+    const dominator::MenuItem* current = &dominator::MAIN;
+    do {
+        current = current->func(current, current->data);
+    } while (true);
+
+    return 0;
+}
