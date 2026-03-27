3231bdca992b6dcbc552c6566b077989be573524
Raivisor <georgijsmirnov159@gmail.com>
2026-03-28 00:07:25 +0300

    Rename directory

---
+++ menu_homework_3/build_module_app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CPP_FILES="main.cpp define_menu_items.cpp menu_function.cpp"
+APP=dominator
+FOLDER=bin
+
+mkdir -p $FOLDER
+
+if [ -f "$FOLDER/$APP" ]; then 
+    rm "$FOLDER/$APP"
+fi
+
+g++ -I. $CPP_FILES -o $FOLDER/$APP
+
+if [ -f "$FOLDER/$APP" ]; then
+    ./$FOLDER/$APP
+else
+    echo "GG - Good Game"
+    exit 1
+fi
