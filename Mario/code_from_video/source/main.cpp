75288d8b32f040fae662b40ac6cb4e43b9ea8884
Raivisor <georgijsmirnov159@gmail.com>
2026-04-27 19:49:14 +0300

    Init mario

--- Mario/code_from_video/source/main.cpp
+++ Mario/code_from_video/source/main.cpp
@@ -1,13 +1,16 @@
 #include <cstdio>
+#include <math.h>
 
 #define mapWidth 86
 #define mapHeight 45
 
-struct Coordinates {
+struct Object {
 	float x, y;
-}
+	float width, height;
+};
 
 char map[mapHeight][mapWidth+1];
+Object mario;
 
 void ClearMap() {
 	for(int i = 0; i < mapWidth; i++){
@@ -26,8 +29,33 @@ void ShowMap() {
 	}
 }
 
+void SetPosition(Object* obj, float xPos, float yPos) {
+	obj->x = xPos;
+	obj->y = yPos;
+}
+
+void InitObj(Object* obj, float xPos, float yPos, float width = 1, float height = 1){
+	SetPosition(obj, xPos, yPos);
+	obj->width = width;
+	obj->height = height;
+}
+
+void PutObjectOnMap(Object* obj) {
+	int ix = round(obj->x);
+	int iy = round(obj->y);
+	int iWidth = round(obj->width);
+	int iHeight = round(obj->height);
+	for(int i = ix; i < (ix + iWidth); i++) {
+		for(int j = iy; j < (iy + iHeight); j++) {
+			map[j][i] = '@';
+		}
+	}
+}
+
 int main() {
+	InitObj(&mario, 10, 20, 3, 3);
 	ClearMap();
+	PutObjectOnMap(&mario);
 	ShowMap();
 	return 0;
 }
