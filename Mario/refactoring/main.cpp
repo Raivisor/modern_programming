d9a718a9370a7bb78e12a7aa3258cb78ec5261e9
Raivisor <georgijsmirnov159@gmail.com>
2026-05-10 19:02:35 +0300

    refactoring logic of brick spawning

--- Mario/refactoring/main.cpp
+++ Mario/refactoring/main.cpp
@@ -6,6 +6,8 @@
 #define MAP_WIDTH 195
 #define MAP_HEIGHT 50
 #define ABS_SPEED 1
+#define ENEMY_SPEED 0.3335
+#define COIN_SPEED 0.5 
 #define JUMP_FORCE 1
 #define JUMP_FORCE_ON_KILL -0.5
 #define GRAVITY 0.05
@@ -27,14 +29,57 @@ struct Object {
 	char type;
 };
 
+struct Brick {
+	float x, y;
+	float width, height;
+	char type;
+};
+
+Brick level1Brick[] = {
+	{20, 20,  40, 5, '#'},
+	{100, 20, 20, 5, '#'},
+	{120, 15, 10, 10, '#'},
+	{150, 20, 40, 5, '#'},
+	{60, 15,  40, 10, '#'},
+
+	{30, 10,  5, 3, '?'},
+	{50, 10,  5, 3, '?'},
+	{60, 5,  10, 3, '-'},
+	{70, 5,  5, 3, '?'},
+	{75, 5,  5, 3, '-'},
+	{80, 5,  5, 3, '?'},
+	{85, 5,  10, 3, '-'},
+
+	{210, 15, 10, 10, '+'}
+};
+
+Brick level2Brick[] = {
+	{20, 20,  40, 5, '#'},
+	{60, 15,  10, 10, '#'},
+	{80, 20, 20, 5, '#'},
+	{120, 15, 10, 10, '#'},
+	{150, 20, 40, 5, '#'},
+	{210, 15, 10, 10, '+'},
+};
+
+Brick level3Brick[] = {
+	{20, 20,  40, 5, '#'},
+	{80, 20,  15, 5, '#'},
+	{120, 15, 15, 10, '#'},
+	{160, 10, 15, 15, '+'},
+};
+
 char map[MAP_HEIGHT][MAP_WIDTH+1];
 Object mario;
 
 Object *brick = nullptr;
 int brickCount;
 
-Object *moving = nullptr;
-int movingCount;
+Object *enemy = nullptr;
+int enemyCount;
+
+Object *coin = nullptr;
+int coinCount;
 
 int level = 1;
 int score; 
@@ -87,13 +132,20 @@ void SetPosition(Object* obj, float xPos, float yPos) {
 	obj->y = yPos;
 }
 
-void InitObj(Object* obj, float xPos, float yPos, char type, float width = 1, float height = 1){
+void InitObj(Object* obj, 
+		float xPos, 
+		float yPos, 
+		char type, 
+		float width = 1, 
+		float height = 1, 
+		float speed = 0
+	) {
 	SetPosition(obj, xPos, yPos);
 	obj->width = width;
 	obj->height = height;
 	obj->verticalSpeed = 0;
 	obj->type = type;
-	obj->horizontalSpeed = 0.2;
+	obj->horizontalSpeed = speed;
 }
 
 Object* GetNewBrick() {
@@ -102,62 +154,61 @@ Object* GetNewBrick() {
 	return brick + brickCount - 1;
 }
 
-Object* GetNewMoving() {
-	movingCount++;
-	moving = (Object*)realloc(moving, sizeof(Object) * movingCount);
-	return moving + movingCount - 1;
+Object* GetNewEnemy() {
+	enemyCount++;
+	enemy = (Object*)realloc(enemy, sizeof(Object) * enemyCount);
+	return enemy + enemyCount - 1;
+}
+
+Object* GetNewCoin() {
+	coinCount++;
+	coin = (Object*)realloc(coin, sizeof(Object) * coinCount);
+	return coin + coinCount - 1;
+}
+
+void LoadBrick(Brick *bricks, int count){
+	for(int i = 0; i < count; i++) {
+		InitObj(GetNewBrick(), 
+				bricks[i].x, 
+				bricks[i].y,
+				bricks[i].type,
+				bricks[i].width, 
+				bricks[i].height);
+	}
 }
 
 void CreateLevel(int level) {
 	brickCount = 0;
 	brick = (Object*)realloc(brick, 0);
-	movingCount = 0;
-	moving = (Object*)realloc(moving, 0);
+	enemyCount = 0;
+	enemy = (Object*)realloc(enemy, 0);
+	coinCount = 0;
+	coin = (Object*)realloc(coin, 0);
 
-	InitObj(&mario, 39, 10, '@', 3, 3);
+	InitObj(&mario, 39, 10, '@', 3, 3, ABS_SPEED);
 	score = 0;
 
 	if(level == 1) {
-		InitObj(GetNewBrick(), 20, 20, '#',  40, 5);
-			InitObj(GetNewBrick(), 30, 10, '?',  5, 3);
-			InitObj(GetNewBrick(), 50, 10, '?',  5, 3);
-		InitObj(GetNewBrick(), 60, 15, '#',  40, 10);
-			InitObj(GetNewBrick(), 60, 5, '-',  10, 3);
-			InitObj(GetNewBrick(), 70, 5, '?',  5, 3);
-			InitObj(GetNewBrick(), 75, 5, '-',  5, 3);
-			InitObj(GetNewBrick(), 80, 5, '?',  5, 3);
-			InitObj(GetNewBrick(), 85, 5, '-',  10, 3);
-		InitObj(GetNewBrick(), 100, 20, '#', 20, 5);
-		InitObj(GetNewBrick(), 120, 15, '#', 10, 10);
-		InitObj(GetNewBrick(), 150, 20, '#', 40, 5);
-		InitObj(GetNewBrick(), 210, 15, '+', 10, 10);
+		LoadBrick(level1Brick, sizeof(level1Brick)/sizeof(level1Brick[0]));
 	} else if(level == 2) {
-		InitObj(GetNewBrick(), 20, 20, '#',  40, 5);
-		InitObj(GetNewBrick(), 60, 15, '#',  10, 10);
-		InitObj(GetNewBrick(), 80, 20, '#', 20, 5);
-		InitObj(GetNewBrick(), 120, 15, '#', 10, 10);
-		InitObj(GetNewBrick(), 150, 20, '#', 40, 5);
-		InitObj(GetNewBrick(), 210, 15, '+', 10, 10);
+		LoadBrick(level2Brick, sizeof(level2Brick)/sizeof(level2Brick[0]));
 		
-		InitObj(GetNewMoving(), 25, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 80, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 65, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 120, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 160, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 175, 10, 'o', 3, 2);
+		InitObj(GetNewEnemy(), 25, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 80, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 65, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 120, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 160, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 175, 10, 'o', 3, 2, ENEMY_SPEED);
 	}
 	else if(level == 3) {
-		InitObj(GetNewBrick(), 20, 20, '#',  40, 5);
-		InitObj(GetNewBrick(), 80, 20, '#',  15, 5);
-		InitObj(GetNewBrick(), 120, 15, '#', 15, 10);
-		InitObj(GetNewBrick(), 160, 10, '+', 15, 15);
+		LoadBrick(level3Brick, sizeof(level3Brick)/sizeof(level3Brick[0]));
 		
-		InitObj(GetNewMoving(), 25, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 50, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 80, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 90, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 120, 10, 'o', 3, 2);
-		InitObj(GetNewMoving(), 130, 10, 'o', 3, 2);
+		InitObj(GetNewEnemy(), 25, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 50, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 80, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 90, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 120, 10, 'o', 3, 2, ENEMY_SPEED);
+		InitObj(GetNewEnemy(), 130, 10, 'o', 3, 2, ENEMY_SPEED);
 	}
 }
 
@@ -173,10 +224,16 @@ void PutObjectOnMap(Object* obj) {
 	}
 }
 
-void DeleteMoving(int i) {
-	movingCount--;
-	moving[i] = moving[movingCount];
-	moving = (Object*)realloc(moving, sizeof(Object) * movingCount);
+void DeleteEnemy(int i) {
+	enemyCount--;
+	enemy[i] = enemy[enemyCount];
+	enemy = (Object*)realloc(enemy, sizeof(Object) * enemyCount);
+}
+
+void DeleteCoin(int i) {
+	coinCount--;
+	coin[i] = coin[coinCount];
+	coin = (Object*)realloc(coin, sizeof(Object) * coinCount);
 }
 
 void PlayerDead() {
@@ -207,8 +264,8 @@ void MoveObjectVertical(Object* obj) {
 
 			if((brick[i].type == '?') && obj->verticalSpeed < 0 && obj == &mario) {
 				brick[i].type = '-';
-				InitObj(GetNewMoving(), brick[i].x, brick[i].y-3, '$', 3, 2);
-				moving[movingCount - 1].verticalSpeed -= COIN_JUMP_FORCE;
+				InitObj(GetNewCoin(), brick[i].x, brick[i].y-3, '$', 3, 2, COIN_SPEED);
+				coin[coinCount - 1].verticalSpeed -= COIN_JUMP_FORCE;
 			}
 
 			obj->y -= obj->verticalSpeed;
@@ -256,33 +313,37 @@ void MoveMapHorizontal(float dx) {
 	for(int i = 0; i < brickCount; i++) {
 		brick[i].x += dx;
 	}
-	for(int i = 0; i < movingCount; i++) {
-		moving[i].x += dx;
+	for(int i = 0; i < enemyCount; i++) {
+		enemy[i].x += dx;
+	}
+	for(int i = 0; i < coinCount; i++) {
+		coin[i].x += dx;
 	}
 }
 
 void MarioCollision() {
-	for(int i = 0; i < movingCount; i++) {
-		if(IsCollision(mario, moving[i])) {
-			if(moving[i].type == 'o'){
-				if(mario.IsFly &&
-				   mario.verticalSpeed > 0 &&
-				   mario.y + mario.height < moving[i].y + moving[i].height*0.5) {
-					DeleteMoving(i);
-					mario.verticalSpeed = JUMP_FORCE_ON_KILL*JUMP_FORCE;
-					MoveObjectVertical(&mario);
-					score += ENEMY_SCORE;
-					i--;
-					continue;	
-				} else { 
-					PlayerDead();
-				}
-			} else if(moving[i].type == '$') {
-				DeleteMoving(i);
-				score += COIN_SCORE;
+	for(int i = 0; i < enemyCount; i++) {
+		if(IsCollision(mario, enemy[i])) {
+			if(mario.IsFly &&
+			   mario.verticalSpeed > 0 &&
+			   mario.y + mario.height < enemy[i].y + enemy[i].height*0.5) {
+				DeleteEnemy(i);
+				mario.verticalSpeed = JUMP_FORCE_ON_KILL;
+				MoveObjectVertical(&mario);
+				score += ENEMY_SCORE;
 				i--;
-				continue;
+				continue;	
+			} else { 
+				PlayerDead();
 			}
+		}
+	}
+	for(int i = 0; i < coinCount; i++) {
+		if(IsCollision(mario, coin[i])) {
+			DeleteCoin(i);
+			score += COIN_SCORE;
+			i--;
+			continue;	
 		}
 	}
 }
@@ -294,12 +355,17 @@ void RenderMap() {
 	for(int i = 0; i < brickCount; i++) {
 		PutObjectOnMap(&brick[i]);
 	}
-	for(int i = 0; i < movingCount; i++) {
-		PutObjectOnMap(&moving[i]);
-		MoveObjectVertical(&moving[i]);
-		MoveObjectHorizontal(&moving[i]);
-		if(moving[i].y > MAP_HEIGHT) {
-			DeleteMoving(i);
+	for(int i = 0; i < enemyCount; i++) {
+		PutObjectOnMap(&enemy[i]);
+		MoveObjectVertical(&enemy[i]);
+		MoveObjectHorizontal(&enemy[i]);
+	}
+	for(int i = 0; i < coinCount; i++) {
+		PutObjectOnMap(&coin[i]);
+		MoveObjectVertical(&coin[i]);
+		MoveObjectHorizontal(&coin[i]);
+		if(coin[i].y > MAP_HEIGHT) {
+			DeleteCoin(i);
 			i--;
 			continue;
 		}
@@ -334,6 +400,7 @@ int main() {
 
 	endwin();
 	free(brick);
-	free(moving);
+	free(enemy);
+	free(coin);
 	return 0;
 }
