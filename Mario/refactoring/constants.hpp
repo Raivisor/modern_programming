dc3ef62487d6cc2bb01d56b3561fa128c981c3af
Raivisor <georgijsmirnov159@gmail.com>
2026-05-11 22:41:39 +0300

    Create file for constants

---
+++ Mario/refactoring/constants.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#define MAP_WIDTH 195
+#define MAP_HEIGHT 50
+#define ABS_SPEED 1
+#define ENEMY_SPEED 0.3335
+#define COIN_SPEED 0.5
+#define JUMP_FORCE 1
+#define JUMP_FORCE_ON_KILL -0.5
+#define GRAVITY 0.05
+#define COIN_JUMP_FORCE -0.7
+#define KEY_A 97
+#define KEY_D 100
+#define ESC 27
+#define SPACE 32
+#define MAX_LEVEL 3
+#define COIN_SCORE 100
+#define ENEMY_SCORE 50
+#define BLOCK_WALL '#'
+#define BLOCK_QUESTION '?'
+#define BLOCK_DASH '-'
+#define BLOCK_DOOR '+'
+#define ENEMY_TYPE 'o'
+#define COIN_TYPE '$'
+#define PLAYER_TYPE '@'
