0b0d3ed7cfaa61c385cc352f8ee9eba498a47f8d
Raivisor <georgijsmirnov159@gmail.com>
2026-05-21 14:22:48 +0300

    Added render

---
+++ Mario/OOP/render.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "entity.hpp"
+
+namespace sea {
+	class Render {
+		private:
+			char** map;
+			int width;
+			int height;
+
+			bool isOnMap(int x, int y) const;
+		public:
+			Render(int width, int height);
+			~Render();
+
+			void clearMap();
+			void putEntityOnMap(sea::Entity& type);
+			void putScoreOnMap(int score);
+			void refreshMap();
+	}
+}
